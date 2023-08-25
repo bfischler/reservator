d8d758e99605a1c4ce0426ca8d469180b5d80d38
Nick Palumbo <shmozer@gmail.com>
2023-08-25 12:21:58 -0400

    Fix script

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # reservator
 
-Thanks to `howardbp/reservator`. Made config JSON-able, and a few other changes.
+Thanks to `howardbp/reservator`. Made config JSON-able, and a few other changes. Note that the api_key in the headers is not a secret, it's a public API key that resy uses on its website.
+
+## Populating Config.json
+Put in your email and password for resy.
+
+For `venue_id`, you can go to the place you'd like to visit on resy.com, and go to the `Network` tab of the developer console in Chrome. You'll see a `venue_id` referenced in many of the requests; use this.
--- resy.py
+++ resy.py
@@ -4,21 +4,11 @@ import time
 import csv
 import json
 import sys
-import pause
 
 
-headers = {
-     'origin': 'https://resy.com',
-     'accept-encoding': 'gzip, deflate, br',
-     'x-origin': 'https://resy.com',
-     'accept-language': 'en-US,en;q=0.9',
-     'authorization': 'ResyAPI api_key="VbWk7s3L4KiK5fzlO7JD3Q5EYolJI7n5"',
-     'content-type': 'application/x-www-form-urlencoded',
-     'accept': 'application/json, text/plain, */*',
-     'referer': 'https://resy.com/',
-     'authority': 'api.resy.com',
-     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36',
-}
+headers = {}
+with open("./headers.json") as f:
+    headers = json.load(f)
 
 def login(username, password):
     data = {
@@ -36,7 +26,7 @@ def find_table(res_date, party_size, table_time, auth_token, venue_id):
     #convert datetime to string
     day = res_date.strftime('%Y-%m-%d')
     params = (
-     ('x-resy-auth-token',  auth_token),
+    #  ('x-resy-auth-token',  auth_token),
      ('day', day),
      ('lat', '0'),
      ('long', '0'),
@@ -44,6 +34,7 @@ def find_table(res_date, party_size, table_time, auth_token, venue_id):
      ('venue_id',str(venue_id)),
     )
     response = requests.get('https://api.resy.com/4/find', headers=headers, params=params)
+    print(str(response.text))
     data = response.json()
     results = data['results']
     if len(results['venues']) > 0:
@@ -122,7 +113,9 @@ def main():
         try:
             for date in dates:
                 day = datetime.datetime.strptime(date, '%m/%d/%Y')
+                print(f"checking for {date} at {table_time}...")
                 reserved = try_table(day, party_size, table_time, auth_token, venue_id, payment_method_string)
+                print(reserved)
 
                 if reserved:
                     print("reservation successful")
@@ -132,9 +125,10 @@ def main():
                     time.sleep(.1)
 
         except Exception as e:
-            with open('failures.csv','a') as outf:
-                writer = csv.writer(outf)
-                writer.writerow([time.time()])
+            pass
+            # with open('failures.csv','a') as outf:
+            #     writer = csv.writer(outf)
+            #     writer.writerow([time.time()])
 
 if __name__ == "__main__":
     main()
